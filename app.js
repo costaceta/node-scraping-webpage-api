e17d2988213f90f7c955d49d52ba4188cd571480
costaceta <rcostadesignweb@gmail.com>
2025-04-25 11:00:25 -0300

    feat: Implentado autenticação com X-API-KEY

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 import express from 'express';
 
 import scrapeRoutes from './routes/scrapeRoutes.js';
+import { authenticateApiKey } from './middleware/authenticateApiKey.js';
 
 const app = express();
 
 // Middleware to parse JSON body
 app.use(express.json());
+
+// Aplica o middleware de autenticação globalmente
+app.use(authenticateApiKey);
 
 // Routes
 app.use('/scrape', scrapeRoutes);
---
+++ middleware/authenticateApiKey.js
@@ -0,0 +1,9 @@
+// Middleware para autenticação com x-api-key
+export const authenticateApiKey = (req, res, next) => {
+  const apiKey = req.headers['x-api-key'];
+  if (apiKey === process.env.API_KEY) {
+    next();
+  } else {
+    res.status(403).json({ error: 'Acesso negado: chave de API inválida' });
+  }
+};
